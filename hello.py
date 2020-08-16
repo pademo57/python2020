eb596a0e731b65adaa692864c2d2ac64d3d1b8c4
Paul Morphy <pademo57@gmail.com>
2020-08-16 13:57:48 -0400

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,31 @@
+# Clear the Git Bash Terminal screen
+import os
+os.system('clear')
+
+full_name = "Paul Morphy"
+
+# Lists
+names = ["John", "Bob", "Mary"]
+
+print (full_name)
+# To print from the list called names 
+print (names[2])
+# This will print out Mary - 0, 1, 2
+
+# Tuples are like lists with ( ) instead of [] and faster
+
+# Dictionary
+
+fav_pizza = {
+	"John": "Pepperoni",
+	"Bob": "Mushroom",
+	"Mary": "Cheese"
+}
+# Dictionary key-value pairs key = John, pair = Pepperoni
+print (fav_pizza["John"])
+
+# Boolean
+
+exist = True
+
+print (exist)
